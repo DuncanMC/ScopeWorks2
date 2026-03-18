d2138a2eb8221dbef5a3ea7aa03224ac4cc8e17f
Duncan Champney <dchampney@wareto.com>
2026-03-18 14:57:30 -0400

    Initial Commit

---
+++ ScopeWorks2/ScopeWorks2App.swift
@@ -0,0 +1,17 @@
+//
+//  ScopeWorks2App.swift
+//  ScopeWorks2
+//
+//  Created by Duncan Champney on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ScopeWorks2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
